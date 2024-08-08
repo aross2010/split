65e9c1d7bd09dc107ba80165aea7ce293b9ae0f5
Alex Ross <adross1027@gmail.com>
2024-08-08 01:33:33 -0700

    #

--- app/functions/get-session.ts
+++ app/functions/get-session.ts
@@ -1,9 +1,11 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { AuthOptions, getServerSession } from 'next-auth'
+import authOptions from '@/app/api/auth/[...nextauth]/route'
 import { Session } from '@/app/libs/types'
 
 export const getSession = async () => {
-  const session = (await getServerSession(authOptions as any)) as Session | null
+  const session = (await getServerSession(
+    authOptions as AuthOptions
+  )) as Session | null
 
   return session
 }
